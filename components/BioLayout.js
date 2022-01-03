da7167302dc546f48e3f8df8867788990877b2b0
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-01-03 16:55:13 +0700

    add seo tag, install swiper module for optimize

--- components/BioLayout.js
+++ components/BioLayout.js
@@ -1,47 +1,48 @@
+import Head from 'next/head'
 import Image from "next/image";
-import Link from "next/link";
-import Script from "next/script"
-import { useEffect } from "react";
+import { Scrollbar, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react'
 import Avatar from "./Avatar";
 import BioFooter from "./BioFooter";
 
 const BioLayout = ({ children }) => {
 
-    useEffect(() => {
-        const swiper = new Swiper('.swiper', {
-            slidesPerView: 1,
-            spaceBetween: 0,
-            scrollbar: {
-                el: '.swiper-scrollbar',
-                draggable: true,
-            }
-        });
-    }, [])
-
     return (
         <>
-            <Script src="https://unpkg.com/swiper@7.3.3/swiper-bundle.min.js" strategy="beforeInteractive" />
-            <div className='swiper bio-background'>
+            <Head>
+                <title> Alfian Nahar | Biopage </title>
+                <meta name="title" content="Alfian Nahar" />
+                <meta name="description" content="A personal own-made bio page for connected to me!" />
+                <meta name="keywords" content="Freelance Web Developer, Fullstack Developer, Frontend Developer, Javascript Developer, React Developer, Next.Js Developer, GatsbyJs Developer" />
+                <meta name="robots" content="index, follow" />
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <meta name="language" content="English" />
+                <meta name="revisit-after" content="1 days" />
+                <meta name="author" content="Alfian Nahar" />
+            </Head>
+            <Swiper
+                // install Swiper modules
+                modules={[Scrollbar, Navigation]}
+                spaceBetween={0}
+                slidesPerView={1}
+                navigation={{ prevEl: '.slidePrev-btn', nextEl: '.btn-nxt' }}
+                scrollbar={{ draggable: true }}
+                className="bio-background"
+            >
                 <div className="bio-logo">
                     <Image src="/an-logo.svg" alt="logo AN" layout="responsive" width={100} height={68.92} />
                 </div>
-                <div className="swiper-wrapper">
-                    <div className="swiper-slide bio-page">
-                        <Avatar />
-                        <h3 className="text-xl font-medium mt-4">Alfian Nahar</h3>
-                        <p className="font-light mt-3 mb-10 tracking-widest leading-6">Full Stack Developer with <br /> Minimal UI/UX Design Skills</p>
-                        { children }
-                    </div>
-
-
-
-                    <div className="swiper-slide bio-page">
-                        <Image src="/me-bg.png" alt="me" width={500} height={500} className="grayscale" />
-                    </div>
-                </div>
+                <SwiperSlide className="bio-page">
+                    <Avatar />
+                    <h3 className="text-xl font-medium mt-4">Alfian Nahar</h3>
+                    <p className="font-light mt-3 mb-10 tracking-widest leading-6">Full Stack Developer with <br /> Minimal UI/UX Design Skills</p>
+                    {children}
+                </SwiperSlide>
+                <SwiperSlide className="bio-page">
+                    <Image src="/me-bg.png" alt="me" width={500} height={500} className="grayscale" />
+                </SwiperSlide>
                 <BioFooter />
-                <div className="swiper-scrollbar"></div>
-            </div>
+            </Swiper>
         </>
     )
 }
