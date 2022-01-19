30a01d55d24554a43856fb640efa9c3e7a67c86e
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-01-19 15:52:52 +0700

    edit SEO meta tag

--- components/Layout.js
+++ components/Layout.js
@@ -6,16 +6,29 @@ const Layout = ({ children }) => {
     return (
         <>
             <Head>
-                <title> Alfian Nahar</title>
-                <meta name="title" content="Alfian Nahar" />
-                <meta name="description" content="A personal portfolio page for connected to me!" />
+                <title>Alfian Nahar | Fullstack Developer | Indonesia</title>
+                <meta name="title" content="Alfian Nahar | Fullstack Developer | Indonesia" />
+                <meta name="description" content="A personal portfolio page for connected to me! Just Someone who can work on in front or back. Sometimes design UI too. Just teach me by challenging me for a work!" />
                 <meta name="keywords" content="Alfian, Alfian Nahar, Freelance Web Developer, Fullstack Developer, Frontend Developer, Javascript Developer, React Developer, Next.Js Developer, GatsbyJs Developer" />
                 <meta name="robots" content="index, follow" />
                 <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
                 <meta name="language" content="English" />
                 <meta name="revisit-after" content="1 days" />
                 <meta name="author" content="Alfian Nahar" />
-                <meta property="og:image" content="/an-logo.svg" key="image"/>
+                
+                {/* Open Graph / Facebook */}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://www.alfianahar.com/" />
+                <meta property="og:title" content="Alfian Nahar | Fullstack Developer | Indonesia" />
+                <meta property="og:description" content="A personal portfolio page for connected to me! Just Someone who can work on in front or back. Sometimes design UI too. Just teach me by challenging me for a work!" />
+                <meta property="og:image" content="/metaimage.jpg" />
+
+                {/* Twitter */}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://www.alfianahar.com/" />
+                <meta property="twitter:title" content="Alfian Nahar | Fullstack Developer | Indonesia" />
+                <meta property="twitter:description" content="A personal portfolio page for connected to me! Just Someone who can work on in front or back. Sometimes design UI too. Just teach me by challenging me for a work!" />
+                <meta property="twitter:image" content="/metaimage.jpg" />
             </Head>
             <header className="z-50 fixed flex justify-center items-center px-6 md:px-12 h-24 w-full">
                 <Navbar />
