9f8e99008d94cc70d9cf2d441626da7f2eb50909
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-02-07 23:58:02 +0700

    fix line-height and spacing

--- components/Hero.js
+++ components/Hero.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 const Hero = () => {
     return (
         <div className="flex flex-col justify-center h-screen">
-            <span className="text-xs font-mono font-medium mb-3 text-[#7CB8FC]">
+            <span className="text-xs font-mono font-medium text-[#7CB8FC] leading-7">
                 Get to know me, I&apos;m
             </span>
-            <h1 className="text-5xl font-semibold">
+            <h1 className="text-5xl font-semibold leading-[4.5rem]">
                 Alfian Nahar
             </h1>
-            <p className="font-normal tracking-wider leading-6 text-sm mt-2 opacity-75">
+            <p className="font-normal tracking-wider leading-6 text-sm mt-2 opacity-60">
             Energetic and Motivated Full Stack Developer. Always on exploring, learning, and practicing a new skill set. <br/>
             Actively seeking a project or full-time job, TL:DR -&gt; <span className="text-[#28C14C] font-medium">Open to Work!</span>
             </p>
