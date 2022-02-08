71a40e8604fb8315989fa3e694edcbad31b20f94
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-02-08 19:21:47 +0700

    edit layout

--- components/Hero.js
+++ components/Hero.js
@@ -12,17 +12,20 @@ const Hero = () => {
       preserveAspectRatio: "xMidYMid slice"
     }
   }
+
   return (
     <div className="flex h-screen flex-col lg:flex-row justify-center items-center" >
-      <div className="flex flex-col md:w-4/5 mt-16 lg:-mr-24 ">
+      <div className="flex flex-col md:w-4/5 mt-4 lg:-mr-24 ">
         <span className="font-mono text-xs md:text-sm font-medium leading-7 md:leading-8 text-[#7CB8FC]">
           Get to know me, I&apos;m
         </span>
         <h1 className="text-5xl md:text-6xl lg:text-7xl font-semibold leading-[4.5rem] md:leading-[5.5rem] lg:leading-[6rem] ">Alfian Nahar</h1>
-        <p className="mt-2 text-sm md:text-base font-normal leading-6 md:leading-7 tracking-wider opacity-60">
-          Energetic and Motivated Full Stack Developer. Always on exploring,
-          learning, and practicing a new skill set. <br />
-          Actively seeking a project or full-time job, TL:DR -&gt;
+        <p className="mt-2 text-sm md:text-base font-normal leading-6 md:leading-7 tracking-wider">
+          <span className="opacity-60">
+            Energetic and Motivated Full Stack Developer. Always on exploring,
+            learning, and practicing a new skill set. <br />
+            Actively seeking a project or full-time job, TL:DR -&gt;
+          </span>
           <span className="font-medium text-[#28C14C] "> Open to Work! </span>
         </p>
         <Link href="/bio">
@@ -32,12 +35,14 @@ const Hero = () => {
           </a>
         </Link>
       </div>
-      <div className="mt-9 lg:w-11/12 h-2/6 md:h-auto scale-110 md:scale-105">
+      <div className="mt-9 lg:mt-14 lg:w-11/12 h-2/6 md:h-auto scale-110 md:scale-105">
         <Lottie
           options={defaultOptions}
+          ariaRole=""
+          isClickToPauseDisabled={true}
         />
       </div>
-    </div >
+    </div>
 
   );
 };
